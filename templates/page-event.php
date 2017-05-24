1dcb5faf3b50f2b98a0dfcba1060af395114d638
Bogdan Bele <bogdan.bele@yahoo.com>
2017-05-25 01:39:40 +0200

    Single Event Page

--- templates/page-event.php
+++ templates/page-event.php
@@ -3,4 +3,34 @@
     <div class="container">
     <h1>Single event</h1>
     </div>
+    <div class="event-page-holder">
+        <div class="event card card-2">
+            <div class="event-left">
+                <div class="event-main-text">
+                <p class="event-text-1"> DormNerd </p>
+                <p class="event-text-2"> 23.04.2017 </p>
+                <p class="event-text-3"> In computer programming, event-driven programming is a programming paradigm in which the flow of the program is determined by events such as user actions (mouse clicks, key presses), sensor outputs, or messages from other programs/threads. </p>
+                </div>
+                
+               
+
+
+
+            </div>
+            <div class="event-right">
+                <p class="event-location">
+                    Location
+                </p>
+                <div class="event-image-holder">
+                    <img class="card" src="dist/images/map1.png">
+                </div>
+                <p class="event-adress-line event-adress-line-1">
+                    Rebaek Sopark 5
+                </p>
+                <p class="event-adress-line event-adress-line-2">
+                    Hvidovre 2560, Denmark
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
