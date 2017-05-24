c110549b7aa80b9d04cea6c88e73dc6d748c6fd3
Bogdan Bele <bogdan.bele@yahoo.com>
2017-05-25 01:56:42 +0200

    Contact Form

--- templates/page-contact.php
+++ templates/page-contact.php
@@ -3,4 +3,20 @@
     <div class="container">
     <h1>Contact</h1>
     </div>
+    <div class="contact-container">
+            <form class="contact-form">
+                <div>
+                    <input type="text" id="name" name="name" class="floating-label">
+                    <label for="name" class="active">Name</label>
+                </div>
+
+                <div>
+                    <input type="text" id="email" name="email" class="floating-label">
+                    <label for="email">Email</label>
+                </div>
+
+                <textarea></textarea><br>
+                <input type="submit" value="Send" class="btn">
+            </form>
+    </div>
 </div>
