7fe2d37fe093bc2f8c6cad31975fecd646856e43
Bogdan Bele <bogdan.bele@yahoo.com>
2017-05-25 18:25:56 +0200

    Sidebar

--- templates/home.php
+++ templates/home.php
@@ -19,7 +19,35 @@
         
     </div>
     <div id="event-container"> 
-    
+    <div id="side-menu" class="right-shadow">
+    <div id="menu-slider" class="card card-1">
+      <div class="menu-section menu-section-1">
+        <p> All </p>
+      </div>
+      <div class="menu-section menu-section-2">
+        <p> Today </p>
+        <p> Tomorrow </p>
+        <p> Date </p>
+      </div>
+      <div class="menu-section menu-section-3">
+        <p> Topic 1 </p>
+        <p> Topic 2 </p>
+        <p> Topic 3 </p>
+      </div>
+      <div class="menu-section menu-section-4">
+        <div class="menu-section-top">
+          <p> 0 </p> <p> No Max </p>
+          </div>
+        <div class="menu-section-bottom">
+          <div class="price-center">
+            <div class="price-range">
+              
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
+  </div>
         </div>
     <?php
     //INSERT FOOTER
