19fadd40a5d581459d70e463030d02d464851a1b
Bogdan Bele <bogdan.bele@yahoo.com>
2017-05-25 12:37:38 +0200

    Parteners Base

--- templates/page-partners.php
+++ templates/page-partners.php
@@ -3,4 +3,27 @@
     <div class="container">
     <h1>Partners</h1>
     </div>
+
+    <div class="parteners-container card card-1">
+    <div class="parteners-center">
+    <div class="partener card card-1">
+    </div>
+    <div class="partener card card-1">
+    </div>
+    <div class="partener card card-1">
+    </div>
+    <div class="partener card card-1">
+    </div>
+   <div class="partener card card-1">
+    </div>
+    <div class="partener card card-1">
+    </div>
+    <div class="partener card card-1">
+    </div>
+    <div class="partener card card-1">
+    </div>
+    <div class="partener card card-1">
+    </div>
+    </div>
+    </div>
 </div>
