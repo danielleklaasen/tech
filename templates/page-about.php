9efd347dc552114732213d5ab4cb37bbf1b54793
danielleklaasen <danielleklaasen@gmail.com>
2017-05-25 23:26:29 +0200

    about page and single event

--- templates/page-about.php
+++ templates/page-about.php
@@ -3,19 +3,34 @@
 
 <div class="wdw wdw-about base-color-bg">
 
+<div class="background-image">
+    <div class="background-image-overlay-dark secondary-overlay">
 
-        <section class="horizontal-vertical-center landing-box about-text highlight-color-bg">
+
+        <section class="horizontal-vertical-center landing-box about-text">
+<!--        <section class="horizontal-vertical-center landing-box about-text highlight-color-bg">-->
+
+
+
 
             <div class="header-space"></div>
-            <h6 class="base-color">Organising events since 2017 <i class="fa fa-angle-right" aria-hidden="true"></i></h6>
-            <h1 class="base-color">TechCPH</h1>
-            <p class="emphasis base-color">
-                Hoodie everyday carry stumptown green juice. Fingerstache tumblr cronut crucifix, trust fund mumblecore woke XOXO tattooed letterpress ennui humblebrag cornhole. Copper mug iPhone helvetica gluten-free mlkshk narwhal, <span class="attention-secondary">we are passionate about coding</span> tbh squid vexillologist pok pok actually woke small batch.</p>
+            <h6 class="highlight-color">Organising events since 2017 <i class="fa fa-angle-right" aria-hidden="true"></i></h6>
+            <h1 class="highlight-color">TechCPH</h1>
+            <p class="emphasis highlight-color">
+                Hoodie everyday carry stumptown green juice. Fingerstache tumblr cronut crucifix, trust fund mumblecore woke XOXO tattooed letterpress ennui humblebrag cornhole. Copper mug iPhone helvetica gluten-free mlkshk narwhal, <span class="attention-highlight">we are passionate about coding</span> tbh squid vexillologist pok pok actually woke small batch.</p>
             <hr>
-            <p class="note base-color">
+            <p class="note highlight-color">
                 Hell of paleo schlitz, quinoa actually flexitarian umami fashion axe helvetica sriracha. Lomo artisan whatever, occupy actually chartreuse literally before they sold out gentrify umami keytar edison bulb cloud bread.
             </p>
+
+
+
+
         </section>
+
+    </div>
+</div>
+
 
     <?php
     //INSERT FOOTER
