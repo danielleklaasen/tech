617ca057a5ff5f99ce774f1c9d240d9aef57df46
Bogdan Bele <bogdan.bele@yahoo.com>
2017-05-25 02:40:17 +0200

    CRUD Events ( base )

--- templates/page-crud-events.php
+++ templates/page-crud-events.php
@@ -3,4 +3,63 @@
     <div class="container">
         <h1>Crud events</h1>
     </div>
-</div>
+
+
+ <div class="table-responsive-vertical shadow-z-1">
+  <!-- Table starts here -->
+  <table id="table" class="table table-hover table-mc-light-blue">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Company</th>
+          <th>Date and Place</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td data-title="ID">1</td>
+          <td data-title="Name">Google</td>
+          <td data-title="Link">26/02/17 - Bella Center
+          </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">2</td>
+          <td data-title="Name">Spotify</td>
+          <td data-title="Link"> 19/03/17 - Fisketorvet Auditorium
+          </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">3</td>
+          <td data-title="Name">Tesla</td>
+          <td data-title="Link">
+             05/04/17  - Bella Center </td>
+          <td data-title="Status">Completed</td>
+        </tr>
+        <tr>
+          <td data-title="ID">4</td>
+          <td data-title="Name">Danske Bank</td>
+          <td data-title="Link"> 15/08/18  - Finans Center</td>
+          <td data-title="Status">Upcoming</td>
+        </tr>
+        <tr>
+          <td data-title="ID">5</td>
+          <td data-title="Name">DSB Copenhagen</td>
+          <td data-title="Link"> 30/08/18 - Kogens Nytrov
+          </td>
+          <td data-title="Status">Upcoming</td>
+        </tr>
+        <tr>
+          <td data-title="ID">6</td>
+          <td data-title="Name">Nordea</td>
+          <td data-title="Link">
+            14/09/18 - Carlsberg Byen
+          </td>
+          <td data-title="Status">Upcoming</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  </div>
